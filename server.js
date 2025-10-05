3fe1ad807b75076c16b343e3914c31d42ecf4e1a
isa4sunah <157133757+isa4sunah@users.noreply.github.com>
2025-10-05 17:06:22 +0100

    server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Serve all static files from the public directory
+app.use(express.static(path.join(__dirname, "public")));
+
+// Default route -> home.html
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "home.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
